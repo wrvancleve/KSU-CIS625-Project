258a4684c284f0207439ef1876fed73d94d2fa72
William Van Cleve <wrvancleve@gmail.com>
2018-11-25 17:31:35 -0600

    Start of application and MWC architecture.

---
+++ StockMarketAnalysis/StockMarketAnalysis/MainWindowController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockMarketAnalysis
+{
+    public class MainWindowController
+    {
+        /// <summary>
+        /// Handle to the MainWindow which is the view for this controller.
+        /// </summary>
+        private IWindow _view;
+
+        public MainWindowController()
+        {
+
+        }
+
+        public void AttachView(IWindow view)
+        {
+            _view = view;
+        }
+    }
+}
