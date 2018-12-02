98566fb8f1173cee826ac5764d5ab1630d019596
Webster <31218426+prometheus1994@users.noreply.github.com>
2018-12-02 17:57:52 -0600

    added stored procedure InsertPreviousAggregateData (#12)

--- StockMarketAnalysis/StockMarketAnalysis/Sql/stored_procedures.sql
+++ StockMarketAnalysis/StockMarketAnalysis/Sql/stored_procedures.sql
@@ -15,8 +15,7 @@ insert [StockData].[RawData](StockCode, StockType, HolderId, HolderCountry, Shar
 values(@StockCode, @StockType, @HolderId, @HolderCountry, @SharesHeld, @PercentageSharedHeld, @Direction, @Value)
 go
 
-exec InsertRawData
-
+/* Insert Prefiltered Data */
 drop procedure if exists [StockData].[InsertPreFilteredData]
 go
 create procedure [StockData].[InsertPrefilteredData]
@@ -30,11 +29,21 @@ create procedure [StockData].[InsertPrefilteredData]
 	@Direction nvarchar(32),
 	@Value float
 as 
-insert [StockData].[RawData](StockCode, StockType, HolderId, HolderCountry, SharesHeld, PercentageSharesHeld, Direction, [Value])
+insert [StockData].[PreFilteredData](CriteriaSetId, StockCode, StockType, HolderId, HolderCountry, SharesHeld, PercentageSharesHeld, Direction, [Value])
 values(@CriteriaSetId, @StockCode, @StockType, @HolderId, @HolderCountry, @SharesHeld, @PercentageSharedHeld, @Direction, @Value)
 go 
 
+/* Insert Previous Aggregate Data */
 drop procedure if exists [StockData].[InsertPreviousAggregateData]
 go
 create procedure [StockData].[InsertPreviousAggregateData]
-	@
+	@CriteriaSetId int,
+	@AggregateKey nvarchar(32),
+	@AggregateSharesHeld float,
+	@AggregatePercentageSharesHeld float, 
+	@AggregateValue float
+as 
+insert [StockData].[PreviousAggregateData](CriteriaSetId, AggregateKey, AggregateSharesHeld, AggregatePercentageSharesHeld, AggregateValue)
+values (@CriteriaSetId, @AggregateKey, @AggregateSharesheld, @AggregatePercentageSharesHeld, @AggregateValue)
+go
+
