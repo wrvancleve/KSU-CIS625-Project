b3fe18062483d16407289b92e8d7efdf59cd18c3
Webster <31218426+prometheus1994@users.noreply.github.com>
2018-12-05 14:00:08 -0500

    Jwebster7 stored procedure work 12.5.18 (#24)
    * procedure: GetCurrentAggregateData() complete, procedure: GetPreviousAggregateData() complete, procedure: GetMaxAggregateData() complete,
    trigger: UpdateMaxAggregateData() in-progress.
    
    * finished the trigger for updating the max aggregate data

--- StockMarketAnalysis/StockMarketAnalysis/Sql/stored_procedures.sql
+++ StockMarketAnalysis/StockMarketAnalysis/Sql/stored_procedures.sql
@@ -49,9 +49,9 @@ where
 go 
 
 /* Insert Current Aggregate Data */
-drop procedure if exists [StockData].[GetAggregateData]
+drop procedure if exists [StockData].[GetCurrentAggregateData]
 go
-create procedure [StockData].[GetAggregateData]
+create procedure [StockData].[GetCurrentAggregateData]
 	@CriteriaSetId int,
 	@AggregateKeys nvarchar(128)
 AS
@@ -67,32 +67,42 @@ WHERE PFD.CriteriaSetId = @CriteriaSetId
 GROUP BY PFD.CriteriaSetId, REPLACE(REPLACE(REPLACE(REPLACE(@AggregateKeys, 'holderid', PFD.HolderId), 'stockcode', PFD.StockCode), 'stocktype', PFD.StockType), 'direction', PFD.Direction)
 go
 
-/* Insert Current Aggregate Data */
-drop procedure if exists [StockData].[InsertCurrentAggregateData]
+/* Get Previous Aggregate Data */
+drop procedure if exists [StockData].[GetPreviousAggregateData]
 go
-create procedure [StockData].[InsertCurrentAggregateData]
+create procedure [StockData].[GetPreviousAggregateData]
 	@CriteriaSetId int,
-	@AggregateKey nvarchar(32),
-	@AggregateSharesHeld float,
-	@AggregatePercentageSharesHeld float, 
-	@AggregateValue float
-as 
-insert [StockData].[CurrentAggregateData](CriteriaSetId, AggregateKey, AggregateSharesHeld, AggregatePercentageSharesHeld, AggregateValue)
-values (@CriteriaSetId, @AggregateKey, @AggregateSharesheld, @AggregatePercentageSharesHeld, @AggregateValue)
+	@AggregateKeys nvarchar(128)
+AS
+insert [StockData].[PreviousAggregateData](CriteriaSetId, AggregateKey, AggregateSharesHeld, AggregatePercentageSharesHeld, AggregateValue)
+select PFD.CriteriaSetId as [CriteriaSetId],
+	REPLACE(REPLACE(REPLACE(REPLACE(@AggregateKeys, 'holderid', PFD.HolderId), 'stockcode', PFD.StockCode), 'stocktype', PFD.StockType), 'direction', PFD.Direction) AS [AggregateKey],
+	sum(PFD.SharesHeld) as [AggregateSharesHeld],
+	sum(PFD.PercentageSharesHeld) as [AggregatePercentageSharesHeld],
+	sum(PFD.[Value]) as [AggregateValue]
+from [StockData].[PreFilteredData] as PFD
+WHERE PFD.CriteriaSetId = @CriteriaSetId
+-- the SQL in the where statement may need to be moved to a HAVING clause
+GROUP BY PFD.CriteriaSetId, REPLACE(REPLACE(REPLACE(REPLACE(@AggregateKeys, 'holderid', PFD.HolderId), 'stockcode', PFD.StockCode), 'stocktype', PFD.StockType), 'direction', PFD.Direction)
 go
 
-/* Insert Max Aggregate Data */
-drop procedure if exists [StockData].[InsertMaxAggregateData]
+/* Get Max Aggregate Data */
+drop procedure if exists [StockData].[GetMaxAggregateData]
 go
-create procedure [StockData].[InsertMaxAggregateData]
+create procedure [StockData].[GetMaxAggregateData]
 	@CriteriaSetId int,
-	@AggregateKey nvarchar(32),
-	@AggregateSharesHeld float,
-	@AggregatePercentageSharesHeld float, 
-	@AggregateValue float
-as 
+	@AggregateKeys nvarchar(128)
+AS
 insert [StockData].[MaxAggregateData](CriteriaSetId, AggregateKey, AggregateSharesHeld, AggregatePercentageSharesHeld, AggregateValue)
-values (@CriteriaSetId, @AggregateKey, @AggregateSharesheld, @AggregatePercentageSharesHeld, @AggregateValue)
+select PFD.CriteriaSetId as [CriteriaSetId],
+	REPLACE(REPLACE(REPLACE(REPLACE(@AggregateKeys, 'holderid', PFD.HolderId), 'stockcode', PFD.StockCode), 'stocktype', PFD.StockType), 'direction', PFD.Direction) AS [AggregateKey],
+	sum(PFD.SharesHeld) as [AggregateSharesHeld],
+	sum(PFD.PercentageSharesHeld) as [AggregatePercentageSharesHeld],
+	sum(PFD.[Value]) as [AggregateValue]
+from [StockData].[PreFilteredData] as PFD
+WHERE PFD.CriteriaSetId = @CriteriaSetId
+-- the SQL in the where statement may need to be moved to a HAVING clause
+GROUP BY PFD.CriteriaSetId, REPLACE(REPLACE(REPLACE(REPLACE(@AggregateKeys, 'holderid', PFD.HolderId), 'stockcode', PFD.StockCode), 'stocktype', PFD.StockType), 'direction', PFD.Direction)
 go
 
 /* NOT FINISHED */
@@ -100,17 +110,41 @@ go
 drop trigger if exists [StockData].[UpdateMaxAggregateData]
 go
 
-create trigger [StockData].[UpdateMaxAggregateData] on [StockData].[CurrentAggregateData]
+create trigger [StockData].[UpdateMaxAggregateData] on [StockData].[MaxAggregateData]
 -- may need to be after insert
-after insert, update 
-as 
+after insert
+as
+if exists 
+	(
+		select MAD.CriteriaSetId as [CriteriaSetId],
+			MAD.AggregateKey as [AggregateKey]
+		from [StockData].[MaxAggregateData] as MAD
+		intersect 
+			select i.CriteriaSetId as [CriteriaSetId],
+				i.AggregateKey as [AggregateKey]
+			from inserted as i
+	)
+begin
+	Insert [StockData].[MaxAggregateData]
+	select CriteriaSetId, AggregateKey, MaxAggregateSharesHeld, MaxAggregatePercentageSharesHeld, MaxAggregateValue
+	from 
+	(
+		Select I.CriteriaSetId, I.AggregateKey, I.AggregateSharesHeld, I.AggregatePercentageSharesHeld, I.AggregateValue
+		from inserted i
+		union all
+		Select MAD.CriteriaSetId, MAD.AggregateKey, MAD.AggregateSharesHeld, MAD.AggregatePercentageSharesHeld, MAD.AggregateValue
+		from [StockData].[MaxAggregateData] MAD
+	) as MaxValues(CriteriaSetId, AggregateKey, MaxAggregateSharesHeld, MaxAggregatePercentageSharesHeld, MaxAggregateValue)
+end
 
-if update(AggregateSharesHeld) or update(AggregatePercentageSharesHeld) or update(AggregateValue)
-	-- here we need to replaces values in the PreviousAggregateData table if they reached certain thresh holds. 
-insert [StockData].[MaxAggregateData]()
-select CAD.CriteriaSetId as [CriteriaSetId],
-	CAD.AggregateSharesHeld as [AggregateShareHeld],
-	CAD.AggregatePercentageSharesHeld as [AggregatePercentageSharesHeld],
-	CAD.AggregateValue as [AggregateValue]
-from [StockData].[CurrentAggregateData] as CAD
--- go?
+else
+	
+begin
+	insert [StockData].[MaxAggregateData](CriteriaSetId, AggregateKey, AggregateSharesHeld, AggregatePercentageSharesHeld, AggregateValue)
+	select i.CriteriaSetId as [CriteriaSetId],
+		i.AggregateKey as [AggregateKey],
+		i.AggregateSharesHeld as [AggregateSharesHeld],
+		i.AggregatePercentageSharesHeld as [AggregatePercentageSharesHeld],
+		i.AggregateValue as [AggregateValue]
+	from inserted as i
+end
