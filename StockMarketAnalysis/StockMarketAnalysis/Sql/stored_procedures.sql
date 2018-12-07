aeaf40d6bedd941ca73ce8769723d97c34b23caa
William Van Cleve <vancleve@ksu.edu>
2018-12-06 18:18:03 -0600

    Almost there

--- StockMarketAnalysis/StockMarketAnalysis/Sql/stored_procedures.sql
+++ StockMarketAnalysis/StockMarketAnalysis/Sql/stored_procedures.sql
@@ -5,10 +5,18 @@
  * Stock Market Analysis Optimization
  */
 
+/* (Done) Clears Raw Data Table : Must be done before work */
+DROP PROCEDURE IF EXISTS [StockData].[ClearRawData]
+GO
+CREATE PROCEDURE [StockData].[ClearRawData]
+AS
+	TRUNCATE TABLE [StockData].[RawData]
+GO
+
+/* (Done) Inserts Raw Data into TABLE */
 drop procedure if exists [StockData].[InsertRawData]
 go
 create procedure [StockData].[InsertRawData]
-	-- stockcode, stocktype, holderid, holercountry, sharesheld, percentagesharesheld, direction, value
 	@StockCode nvarchar(32),
 	@StockType nvarchar(32),
 	@HolderId nvarchar(32),
@@ -22,7 +30,7 @@ insert [StockData].[RawData](StockCode, StockType, HolderId, HolderCountry, Shar
 values(@StockCode, @StockType, @HolderId, @HolderCountry, @SharesHeld, @PercentageSharesHeld, @Direction, @Value)
 go
 
-/* Get the data from RawData that meets the condition of the pre-filter and put it into PreFilteredData */
+/* (Done) Get the data from RawData that meets the condition of the pre-filter and put it into PreFilteredData */
 drop procedure if exists [StockData].[GetPreFilterData]
 go
 create procedure [StockData].[GetPreFilterData]
@@ -46,9 +54,24 @@ where
 	(@HolderCountries = 'null' OR CHARINDEX(HolderCountry, @HolderCountries) > 0)
 	AND (@StockType = 'null' OR RD.StockType = @StockType)
 	and (@Direction = 'null' OR RD.Direction = @Direction)
-go 
+GO 
 
-/* Insert Current Aggregate Data */
+/* (Done) Move current aggregate data into previous aggregate data */
+DROP PROCEDURE IF EXISTS [StockData].[MoveAggregateData]
+GO
+CREATE PROCEDURE [StockData].[MoveAggregateData]
+	@CriteriaSetId INT
+AS
+	DELETE FROM [StockData].[PreviousAggregateData]
+	WHERE CriteriaSetId = @CriteriaSetId
+
+	INSERT [StockData].[PreviousAggregateData]
+	SELECT *
+	FROM [StockData].[CurrentAggregateData] CAD
+	WHERE CriteriaSetId = @CriteriaSetId
+Go
+
+/* (Done) Get Current Aggregate Data */
 drop procedure if exists [StockData].[GetCurrentAggregateData]
 go
 create procedure [StockData].[GetCurrentAggregateData]
@@ -67,7 +90,7 @@ WHERE PFD.CriteriaSetId = @CriteriaSetId
 GROUP BY PFD.CriteriaSetId, REPLACE(REPLACE(REPLACE(REPLACE(@AggregateKeys, 'holderid', PFD.HolderId), 'stockcode', PFD.StockCode), 'stocktype', PFD.StockType), 'direction', PFD.Direction)
 go
 
-/* Get Previous Aggregate Data */
+/* (Done) Get Get Aggregate Data */
 drop procedure if exists [StockData].[GetPreviousAggregateData]
 go
 create procedure [StockData].[GetPreviousAggregateData]
@@ -86,14 +109,17 @@ WHERE PFD.CriteriaSetId = @CriteriaSetId
 GROUP BY PFD.CriteriaSetId, REPLACE(REPLACE(REPLACE(REPLACE(@AggregateKeys, 'holderid', PFD.HolderId), 'stockcode', PFD.StockCode), 'stocktype', PFD.StockType), 'direction', PFD.Direction)
 go
 
-/* Get Max Aggregate Data */
+/* (Done) Get Max Aggregate Data */
 drop procedure if exists [StockData].[GetMaxAggregateData]
 go
 create procedure [StockData].[GetMaxAggregateData]
 	@CriteriaSetId int,
 	@AggregateKeys nvarchar(128)
 AS
-insert [StockData].[MaxAggregateData](CriteriaSetId, AggregateKey, AggregateSharesHeld, AggregatePercentageSharesHeld, AggregateValue)
+
+WITH SourceCte(CriteriaSetId, AggregateKey, AggregateSharesHeld, AggregatePercentageSharesHeld, AggregateValue)
+AS
+(
 select PFD.CriteriaSetId as [CriteriaSetId],
 	REPLACE(REPLACE(REPLACE(REPLACE(@AggregateKeys, 'holderid', PFD.HolderId), 'stockcode', PFD.StockCode), 'stocktype', PFD.StockType), 'direction', PFD.Direction) AS [AggregateKey],
 	sum(PFD.SharesHeld) as [AggregateSharesHeld],
@@ -101,50 +127,167 @@ select PFD.CriteriaSetId as [CriteriaSetId],
 	sum(PFD.[Value]) as [AggregateValue]
 from [StockData].[PreFilteredData] as PFD
 WHERE PFD.CriteriaSetId = @CriteriaSetId
--- the SQL in the where statement may need to be moved to a HAVING clause
 GROUP BY PFD.CriteriaSetId, REPLACE(REPLACE(REPLACE(REPLACE(@AggregateKeys, 'holderid', PFD.HolderId), 'stockcode', PFD.StockCode), 'stocktype', PFD.StockType), 'direction', PFD.Direction)
-go
+)
 
-/* NOT FINISHED */
-/* Trigger to insert Max Aggregate Data */
-drop trigger if exists [StockData].[UpdateMaxAggregateData]
-go
+MERGE [StockData].[MaxAggregateData] MAD
+USING SourceCte SCTE ON SCTE.CriteriaSetId = MAD.CriteriaSetId AND SCTE.AggregateKey = MAD.AggregateKey
+WHEN MATCHED THEN
+	UPDATE
+	SET
+		AggregateSharesHeld = 
+		(
+			SELECT MAX(AggregateSharesHeld)
+			FROM
+			(
+				SELECT MAD2.AggregateSharesHeld
+				FROM [StockData].[MaxAggregateData] MAD2
+				WHERE MAD2.CriteriaSetId = MAD.CriteriaSetId AND MAD2.AggregateKey = MAD.AggregateKey
+				UNION ALL
+				SELECT SCTE.AggregateSharesHeld
+				FROM SCTE
+				WHERE SCTE.CriteriaSetId = MAD.CriteriaSetId AND SCTE.AggregateKey = MAD.AggregateKey
+			) AS SharesHeld
+		),
+		AggregatePercentageSharesHeld = 
+		(
+			SELECT MAX(AggregatePercentageSharesHeld)
+			FROM
+			(
+				SELECT MAD2.AggregatePercentageSharesHeld
+				FROM [StockData].[MaxAggregateData] MAD2
+				WHERE MAD2.CriteriaSetId = MAD.CriteriaSetId AND MAD2.AggregateKey = MAD.AggregateKey
+				UNION ALL
+				SELECT SCTE.AggregatePercentageSharesHeld
+				FROM SCTE
+				WHERE SCTE.CriteriaSetId = MAD.CriteriaSetId AND SCTE.AggregateKey = MAD.AggregateKey
+			) AS PercentageSharesHeld
+		),
+		AggregateValue = 
+		(
+			SELECT MAX(AggregateValue)
+			FROM
+			(
+				SELECT MAD2.AggregateValue
+				FROM [StockData].[MaxAggregateData] MAD2
+				WHERE MAD2.CriteriaSetId = MAD.CriteriaSetId AND MAD2.AggregateKey = MAD.AggregateKey
+				UNION ALL
+				SELECT SCTE.AggregateValue
+				FROM SCTE
+				WHERE SCTE.CriteriaSetId = MAD.CriteriaSetId AND SCTE.AggregateKey = MAD.AggregateKey
+			) AS PercentageSharesHeld
+		)
+WHEN NOT MATCHED THEN
+	INSERT (CriteriaSetId, AggregateKey, AggregateSharesHeld, AggregatePercentageSharesHeld, AggregateValue)
+	VALUES (SourceCte.CriteriaSetId, SourceCte.AggregateKey, SourceCte.AggregateSharesHeld, SourceCte.AggregatePercentageSharesHeld, SourceCte.AggregateValue);
+GO
 
-create trigger [StockData].[UpdateMaxAggregateData] on [StockData].[MaxAggregateData]
--- may need to be after insert
-after insert
-as
-if exists 
+
+
+drop procedure if exists [StockData].[GetPostFilterData]
+go
+create procedure [StockData].[GetPostFilterData]
+	@CriteriaSetId int,
+	@ColumnName NVARCHAR(128),
+	@Comparison NVARCHAR(128),
+	@Value nvarchar(128)
+AS
+
+IF OBJECT_ID('[StockData].#PostFilterData') IS NULL
+	CREATE TABLE [StockData].#PostFilterData
 	(
-		select MAD.CriteriaSetId as [CriteriaSetId],
-			MAD.AggregateKey as [AggregateKey]
-		from [StockData].[MaxAggregateData] as MAD
-		intersect 
-			select i.CriteriaSetId as [CriteriaSetId],
-				i.AggregateKey as [AggregateKey]
-			from inserted as i
+		PostFilterId int not null identity(1,1) primary key clustered,
+		CriteriaSetId int not null,
+		AggregateKey nvarchar(128) not null,
+		AggregateSharesHeld float not null,
+		AggregatePercentageSharesHeld float not null, 
+		AggregateValue float not null
 	)
-begin
-	Insert [StockData].[MaxAggregateData]
-	select CriteriaSetId, AggregateKey, MaxAggregateSharesHeld, MaxAggregatePercentageSharesHeld, MaxAggregateValue
-	from 
-	(
-		Select I.CriteriaSetId, I.AggregateKey, I.AggregateSharesHeld, I.AggregatePercentageSharesHeld, I.AggregateValue
-		from inserted i
-		union all
-		Select MAD.CriteriaSetId, MAD.AggregateKey, MAD.AggregateSharesHeld, MAD.AggregatePercentageSharesHeld, MAD.AggregateValue
-		from [StockData].[MaxAggregateData] MAD
-	) as MaxValues(CriteriaSetId, AggregateKey, MaxAggregateSharesHeld, MaxAggregatePercentageSharesHeld, MaxAggregateValue)
-end
 
-else
-	
-begin
-	insert [StockData].[MaxAggregateData](CriteriaSetId, AggregateKey, AggregateSharesHeld, AggregatePercentageSharesHeld, AggregateValue)
-	select i.CriteriaSetId as [CriteriaSetId],
-		i.AggregateKey as [AggregateKey],
-		i.AggregateSharesHeld as [AggregateSharesHeld],
-		i.AggregatePercentageSharesHeld as [AggregatePercentageSharesHeld],
-		i.AggregateValue as [AggregateValue]
-	from inserted as i
-end
+	/*
+SET @ColumnName = CONCAT(@ColumnName, ' ', @Comparison, ' ', @Value)
+SET @NewColumn = CONCAT('ALTER TABLE [StockData].#PostFilterData ADD COLUMN `' , @ColumnName, '`NVARCHAR(128)');
+PREPARE stmt FROM @NewColumn;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+*/
+
+IF @Comparison = 'MAX'
+BEGIN
+	UPDATE [StockData].#PostFilterData
+	SET NewColumn = 'true'
+	WHERE EXISTS
+	(
+		SELECT CAD.@ColumnName
+		FROM [StockData].[CurrentAggregateData] CAD, [StockData].[MaxAggregateData] MAD
+		WHERE MAD.@ColumnName < @Value AND CAD.@ColumnName > @Value
+	)
+END
+
+ELSE IF @Comparison = 'CROSSES'
+BEGIN
+	UPDATE [StockData].#PostFilterData
+	SET NewColumn = 'true'
+	WHERE EXISTS
+	(
+		SELECT CAD.@ColumnName
+		FROM [StockData].[CurrentAggregateData] CAD, [StockData].[PreviousAggregateData] PAD
+		WHERE PAD.@ColumnName < @Value AND CAD.@ColumnName > @Value
+	)
+END
+
+ELSE IF @Comparison = '<'
+BEGIN
+	UPDATE [StockData].#PostFilterData
+	SET NewColumn = 'true'
+	WHERE EXISTS
+	(
+		SELECT CAD.@ColumnName
+		FROM [StockData].[CurrentAggregateData] CAD
+		WHERE CAD.@ColumnName < @Value
+	)
+END
+
+ELSE IF @Comparison = '<='
+BEGIN
+	UPDATE [StockData].#PostFilterData
+	SET
+		NewColumn = 'true'
+	WHERE EXISTS
+	(
+		SELECT CAD.@ColumnName
+		FROM [StockData].[CurrentAggregateData] CAD
+		WHERE CAD.@ColumnName <= @Value
+	)
+END
+
+ELSE IF @Comparison = '>'
+BEGIN
+	UPDATE [StockData].#PostFilterData
+	SET
+		NewColumn = 'true'
+	WHERE EXISTS
+	(
+		SELECT CAD.@ColumnName
+		FROM [StockData].[CurrentAggregateData] CAD
+		WHERE CAD.@ColumnName > @Value
+	)
+END
+
+ELSE IF @Comparison = '>='
+BEGIN
+	UPDATE [StockData].#PostFilterData
+	SET
+		NewColumn = 'true'
+	WHERE EXISTS
+	(
+		SELECT CAD.@ColumnName
+		FROM [StockData].[CurrentAggregateData] CAD
+		WHERE CAD.@ColumnName >= @Value
+	)
+END
+
+SELECT *
+FROM [StockData].#PostFilterData
+
+GO
